33a2ebf520e5ce838440eac604861647f0c2abca
Julius Suominen <julius.suominen@gmail.com>
2017-09-09 10:42:36 +0200

    added header component 1st version

--- src/app/components/homepage/homepage-header/homepage-header.component.ts
+++ src/app/components/homepage/homepage-header/homepage-header.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-homepage-header',
   templateUrl: './homepage-header.component.html',
   styleUrls: ['./homepage-header.component.css']
 })
-export class HomepageHeaderComponent implements OnInit {
+export class HomepageHeaderComponent {
   @Input() headerData;
 
   constructor() { }
 
-  ngOnInit() {
+  selectNavLink(event) {
+
+    const navItems = document.querySelectorAll('.nav-item');
+
+    Array.from(navItems).forEach(function(item) {
+      if(item.classList.contains('li-selected')) {
+        item.classList.remove('li-selected');
+      }
+    });
+
+    event.target.classList.toggle('li-selected');
   }
 
 }
