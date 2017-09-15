2b0a0ddb19507b8ee90fdf5d7d90463947645e6a
Julius Suominen <julius.suominen@gmail.com>
2017-09-15 15:43:53 +0200

    separated homepage component into own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,23 @@
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HomepageHeaderComponent } from './components/homepage/homepage-header/homepage-header.component';
-import { HomepageIconsComponent } from './components/homepage/homepage-icons/homepage-icons.component';
-import { HomepageWhatisComponent } from './components/homepage/homepage-whatis/homepage-whatis.component';
-import { HomepageComparisonComponent } from './components/homepage/homepage-comparison/homepage-comparison.component';
-import { HomepageSimpleProcessComponent } from './components/homepage/homepage-simple-process/homepage-simple-process.component';
-import { HomepageCustomersSayComponent } from './components/homepage/homepage-customers-say/homepage-customers-say.component';
-import { HomepageInBetaComponent } from './components/homepage/homepage-in-beta/homepage-in-beta.component';
-import { HomepageSecurityComponent } from './components/homepage/homepage-security/homepage-security.component';
-import { HomepageTeamComponent } from './components/homepage/homepage-team/homepage-team.component';
-import { HomepageContactComponent } from './components/homepage/homepage-contact/homepage-contact.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
 import { appRouting } from './app.routing';
 
-import { CmsContentService } from './services/cms-content.service';
+import { HomepageModule } from './components/homepage/homepage.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    NotFoundComponent,
-    HomepageHeaderComponent,
-    HomepageIconsComponent,
-    HomepageWhatisComponent,
-    HomepageComparisonComponent,
-    HomepageSimpleProcessComponent,
-    HomepageCustomersSayComponent,
-    HomepageInBetaComponent,
-    HomepageSecurityComponent,
-    HomepageTeamComponent,
-    HomepageContactComponent,
-    FooterComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    FormsModule,
+    HomepageModule,
     appRouting
   ],
-  providers: [
-    CmsContentService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/homepage/homepage.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { FooterComponent } from '../footer/footer.component';
+import { HomepageComponent } from './homepage.component';
+import { HomepageHeaderComponent } from './homepage-header/homepage-header.component';
+import { HomepageIconsComponent } from './homepage-icons/homepage-icons.component';
+import { HomepageWhatisComponent } from './homepage-whatis/homepage-whatis.component';
+import { HomepageComparisonComponent } from './homepage-comparison/homepage-comparison.component';
+import { HomepageSimpleProcessComponent } from './homepage-simple-process/homepage-simple-process.component';
+import { HomepageCustomersSayComponent } from './homepage-customers-say/homepage-customers-say.component';
+import { HomepageInBetaComponent } from './homepage-in-beta/homepage-in-beta.component';
+import { HomepageSecurityComponent } from './homepage-security/homepage-security.component';
+import { HomepageTeamComponent } from './homepage-team/homepage-team.component';
+import { HomepageContactComponent } from './homepage-contact/homepage-contact.component';
+
+import { CmsContentService } from '../../services/cms-content.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [
+    HomepageComponent,
+    HomepageHeaderComponent,
+    HomepageIconsComponent,
+    HomepageWhatisComponent,
+    HomepageComparisonComponent,
+    HomepageSimpleProcessComponent,
+    HomepageCustomersSayComponent,
+    HomepageInBetaComponent,
+    HomepageSecurityComponent,
+    HomepageTeamComponent,
+    HomepageContactComponent,
+    FooterComponent
+  ],
+  providers: [
+    CmsContentService
+  ]
+})
+export class HomepageModule { }
